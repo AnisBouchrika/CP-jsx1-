f651aec44b8846790804d2d39b78594ba8d5fffb
AnisBouchrika <anice.bouchrika@gmail.com>
2020-11-14 11:27:38 +0100

    checkpoint-jsx-

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React from 'react';
 
-import 'bootstrap/dist/css/bootstrap.css';
+import './styles.css';
+
+import imageInSrc from "./imageInSrc.jpg"
+
 
 
 function App() {
   return (
-    <section>
-      <h1 className="bigtitle">React js</h1>
-      <h3>Login to learn</h3>
-      <form class="form-inline" action="/action_page.php">
-    <div class="form-group">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email"/>
+  <div className="App">
+
+    <div className="container">
+
+        <h1  className="title-red" >ANIS</h1>
+
+
+
+        <img className='image' src={imageInSrc}  alt='imageInSrc' />
+
+
+
+        <img className='image' src="/imageInPublic.jpg" alt='imageInPublic'/>
+        <iframe className="vid1" width="320" height="240" src="https://www.youtube.com/embed/rUWxSEwctFU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd"/>
-    </div>
-    
-    <button type="submit" class="btn-btn-default">Submit</button>
-  </form>
-  </section>
+
+  </div>
   );
 }
 
